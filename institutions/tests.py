2cbf8290674d9671fc7dcc22b9dc9b271c71008e
rei <rei@localhost.localdomain>
2026-02-04 23:11:47 -0500

    Agregando tests model Institution

--- institutions/tests.py
+++ institutions/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from institutions.models import Institution
 
-# Create your tests here.
+class InstitutionTest(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='password123'
+        )
+        
+        self.institution = Institution.objects.create(
+            name="INSTITUTION TEST",
+            emblem="https://emblem.tst.com",
+            description="TEST DESCRIPTION",
+            user=self.user
+        )
+    
+    def test_institution_creation(self):
+        self.assertEqual(str(self.institution), "INSTITUTION TEST")
