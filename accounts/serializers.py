3f0fbdd6615b70cc5866c378c61957f8576e3c6a
rei <rei@localhost.localdomain>
2026-02-12 22:13:55 -0500

    Agregando auth

---
+++ accounts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'confirm_password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+    
+    def validate(self, data):
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("password not match!!")
+        return data
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    
+    def validate(self, data):
+        try:
+            user = User.objects.get(username=data.get('username'))
+        except User.DoesNotExist:
+            raise serializers.ValidationError("El usuario no existe.")
+
+        if not user.check_password(data.get('password')):
+            raise serializers.ValidationError("Contraseña incorrecta.")
+
+        data['user'] = user
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from accounts.serializers import RegisterSerializer, LoginSerializer
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serilizer = LoginSerializer(data=request.data)
+        
+        if serilizer.is_valid():
+            user = serilizer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+            
+            return Response({
+                "message": "Login exitoso",
+                "path": request.path,
+                "status": status.HTTP_200_OK,
+                "data": {
+                    "token": token.key
+                }
+            }, status=status.HTTP_200_OK)
+            
+        return Response({
+            "message": "Login fallido",
+            "path": request.path,
+            "status": status.HTTP_200_OK,
+            "data": serilizer.data
+        }, status=status.HTTP_200_OK)
+
+class RegisterView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.create(user=user)
+            return Response({
+                "message": "Registro exitoso",
+                "path": request.path,
+                "status": status.HTTP_201_CREATED,
+                "data": {
+                    "token": token.key
+                }
+            }, status=status.HTTP_201_CREATED)
+            
+        return Response({
+                "message": "Registro fallido",
+                "path": request.path,
+                "status": status.HTTP_400_BAD_REQUEST,
+                "data": serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        return Response({
+                "message": "Logout realizado",
+                "path": request.path,
+                "status": status.HTTP_200_OK,
+                "data": None
+            }, status=status.HTTP_200_OK)       
